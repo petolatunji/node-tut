83d965ed9cd45d5abca97a39606e513942648991
petolatunji <peterolatunji1@gmail.com>
2022-05-31 04:43:25 -0700

    first commit

---
+++ 2-global.js
@@ -0,0 +1,13 @@
+// GLOBALS - NO WINDOW !!!!
+
+// __dirname  - path to current directory
+// __filename - filename
+// require   - function to use modules (CommonJs)
+// module    - info about current module (file)
+// process   - info about env where the program is being executed
+
+console.log(process)
+
+setInterval(() =>{
+console.log("hello world")
+}, 1000)
---
+++ 8-modules.js
@@ -0,0 +1,10 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated code (only share minimum)
+
+const names = require ("./4-name")
+const sayHi = require("./5-utilis")
+const data = require ("./6-alternative flavor")
+require("./7-mind-grenade")
+sayHi("susan")
+sayHi("john")
+sayHi("peter")
